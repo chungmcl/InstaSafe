98f6d09ba10f36c6b1aab94bf049ba59159fc49f
Micheal Chung <chungmcl@hotmail.com>
2018-11-10 16:36:58 -0800

    Starting

--- InstaSafe/MainWindow.xaml.cs
+++ InstaSafe/MainWindow.xaml.cs
@@ -20,9 +20,41 @@ namespace InstaSafe
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<string[]> suspects;
         public MainWindow()
         {
-            InitializeComponent();
+            this.InitializeComponent();
+        }
+
+        private void ButtonGenerateData_Click(object sender, RoutedEventArgs e)
+        {
+            string[] usernames = this.TextBoxUsernames.Text.Split(',');
+            foreach (string username in usernames)
+            {
+                username.Trim();
+            }
+            
+
+            // Pass all usernames to python algorithm
+            // Save all usernames to text file
+        }
+
+        private void LoadData()
+        {
+
+        }
+
+        private void CalculateSeverity()
+        {
+            for (int i = 0; i < suspects.Count; i++)
+            {
+                suspects[i][i]
+            }
+        }
+
+        private void PostData()
+        {
+
         }
     }
 }
