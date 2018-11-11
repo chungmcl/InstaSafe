2a13f4f3aa57e3627f12a78638e2c65301e6df75
Tim Herron <TimHerronBSA622@outlook.com>
2018-11-10 17:11:09 -0800

    Made the post struct

--- InstaSafe/MainWindow.xaml.cs
+++ InstaSafe/MainWindow.xaml.cs
@@ -21,6 +21,7 @@ namespace InstaSafe
     public partial class MainWindow : Window
     {
         List<string[]> suspects;
+        List<Post> posts;
         public MainWindow()
         {
             this.InitializeComponent();
@@ -55,6 +56,83 @@ namespace InstaSafe
         private void PostData()
         {
 
+        }
+
+        private int SeverityOfPost()
+        {
+            return 0;
+        }
+    }
+
+    struct Post
+    {
+        /// <summary>
+        /// Whether the caption is considered to have risk or not
+        /// </summary>
+        private bool captionBad;
+
+        /// <summary>
+        /// The severity score of the image posted
+        /// </summary>
+        private double imageSeverity;
+
+        /// <summary>
+        /// The date the post was made on.
+        /// </summary>
+        private DateTime date;
+
+        /// <summary>
+        /// Sets the caption severity as a true or false
+        /// </summary>
+        /// <param name="setTo">Whether the caption contains severity</param>
+        public void SetCaption(bool setTo)
+        {
+            this.captionBad = setTo;
+        }
+
+        /// <summary>
+        /// Sets the severity score of the image posted
+        /// </summary>
+        /// <param name="setTo">The severity score of the image</param>
+        public void SetImage(double setTo)
+        {
+            this.imageSeverity = setTo;
+        }
+
+        /// <summary>
+        /// Sets the date of the post
+        /// </summary>
+        /// <param name="setTo">The date the post was made</param>
+        public void SetDate(DateTime setTo)
+        {
+            this.date = setTo;
+        }
+
+        /// <summary>
+        /// Returns whether the caption was risky or not
+        /// </summary>
+        /// <returns>Whether the caption is risky</returns>
+        public bool GetCaption()
+        {
+            return this.captionBad;
+        }
+
+        /// <summary>
+        /// Returns the severity score of the image
+        /// </summary>
+        /// <returns>The severity score of the image</returns>
+        public double GetImage()
+        {
+            return this.imageSeverity;
+        }
+
+        /// <summary>
+        /// Returns the date of the post
+        /// </summary>
+        /// <returns>The date of the post</returns>
+        public DateTime GetDate()
+        {
+            return this.date;
         }
     }
 }
